00d072028e8cceea42c83c45bd3f3e6b72897fe2
Bekir Piralp <bpir33@gmail.com>
2021-12-20 01:18:21 +0300

    Lochal veri tabanı ayarlanmaya başlandı

---
+++ Market.DataAccess/Concrete/LochalMarket/EntityFramework/LochalMarketContext.cs
@@ -0,0 +1,25 @@
+﻿using Market.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.DataAccess.Concrete.LochalMarket.EntityFramework
+{
+    public class LochalMarketContext : DbContext
+    {
+        public DbSet<Bayi> Bayiler { get; set; }
+        public DbSet<Defo> Defolar { get; set; }
+        public DbSet<DepoEkleUrun> DepoEkleUrunler { get; set; }
+        public DbSet<EkBilgi> EkBilgiler { get; set; }
+        public DbSet<Firma> Firmalar { get; set; }
+        public DbSet<Fis> Fisler { get; set; }
+        public DbSet<Iade> Iadeler { get; set; }
+        public DbSet<Personel> Personeller { get; set; }
+        public DbSet<PersonelSifre> PersonelSifreler { get; set; }
+        public DbSet<Satis> Satislar { get; set; }
+        public DbSet<Ulke> Ulkeler { get; set; }
+    }
+}
---
+++ Market.Entity/Concrete/DepoEkleUrun.cs
@@ -0,0 +1,16 @@
+﻿using Market.Entity.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.Entity.Concrete
+{
+    public class DepoEkleUrun : IEntity
+    {
+        public int Id { get; set; }
+        public int Urun { get; set; }
+        public int Adet { get; set; }
+    }
+}
