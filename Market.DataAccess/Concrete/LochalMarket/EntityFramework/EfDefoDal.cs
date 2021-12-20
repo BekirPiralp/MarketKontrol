b44fa1d05a20332f0716f7947e41519e3ac8a8d0
Bekir Piralp <bpir33@gmail.com>
2021-12-20 14:08:10 +0300

    Lochal Market veri tabanı temeller ayarlandı

---
+++ Market.DataAccess/Concrete/LochalMarket/EntityFramework/EfDefoDal.cs
@@ -0,0 +1,14 @@
+﻿using Market.DataAccess.Abstract;
+using Market.Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Market.DataAccess.Concrete.LochalMarket.EntityFramework
+{
+    public class EfDefoDal : EfEntityRepositoryBase<Defo,LochalMarketContext>,IDefoDal
+    {
+    }
+}
